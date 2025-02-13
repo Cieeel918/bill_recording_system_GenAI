270ddf35ce7845e5b471676d961698babfcb6182
Ivanhandsome777 <83528914+Ivanhandsome777@users.noreply.github.com>
2025-02-13 15:41:50 +0000

    [update] delete the csv file and add it in functions.py

--- functions.py
+++ functions.py
@@ -8,7 +8,78 @@ import plotly.express as px
 import os
 from io import StringIO
 
-def create_sample_data():
+
+def prepare_prompt(user_prompt,history_info):
+    '''收入多少，各类型支出多少，'''
+    df=history_info
+    df['timestamp'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+    df["month"]=df["timestamp"].apply(lambda x:x.month)
+    df_spend = df.groupby(["month","income_or_spending"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==0""")
+    df_income= df.groupby(["month","income_or_spending"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==1""")
+    
+    text_all=""  #每月支出和收入
+    for i in range(df_income.shape[0]):
+        income=df_income.iloc[i,:]
+        spend=df_spend.iloc[i,:]
+        text_unit=f"In month {i+1}, the spending is {spend[-1]} and the income is {income[-1]}. "
+        text_all += text_unit
+    
+    df_income_type = df.groupby(["income_or_spending","type"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==1""")
+    df_spend_type = df.groupby(["income_or_spending","type"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==0""")
+
+    income_type_info='' #不同收入类型
+    for i in range(df_income_type.shape[0]):
+        info=df_income_type.iloc[i,:]
+        type=f'The income type is {info[-2]}, the amount is {info[-1]}.'
+        income_type_info+=type
+
+    spend_type_info='' #不同支出类型
+    for i in range(df_spend_type.shape[0]):
+        info=df_spend_type.iloc[i,:]
+        type=f'The spending type is {info[-2]}, the amount is {info[-1]}.'
+        spend_type_info+=type
+
+    
+    prompt = "here is my monthly income and spending information:" + "\n" + text_all + "\n" + "here is my income type information" + income_type_info + "here is my spend type information" + spend_type_info
+    prompt = "here is my goal: " + user_prompt
+    return prompt
+
+
+
+
+def chat_with_gpt(user_input):
+    
+    '''
+    description: 
+        this function call openai api to make users able to interact with chatgpt within this website
+
+    parameters:
+        None
+    
+    output:
+        print the response from chat-gpt
+
+    '''
+    client = OpenAI(
+    api_key=os.getenv("api_key"),
+    )
+    conversation = [{"role": "system", "content": "You are a helpful assistant. And you need to give the user detailed financial plan to achieve his goal."}]
+    try:
+        conversation.append({"role": "user", "content": user_input})
+        chat_completion = client.chat.completions.create(
+            messages=conversation,
+            model="gpt-4o-mini",
+                    )
+        assistant_reply = chat_completion.choices[0].message.content
+        conversation.append({"role": "assistant", "content": assistant_reply})
+        return assistant_reply
+
+    except TimeoutError:
+        print("error")
+
+
+
+def load_data():
     data_sample = """
 timestamp,income_or_spending,type,amount
 2024-01-01,0,Healthcare,1806.08
@@ -379,80 +450,6 @@ timestamp,income_or_spending,type,amount
 2024-12-31,0,Healthcare,1527.29
 """
     df = pd.read_csv(StringIO(data_sample))
-    return df
-
-def prepare_prompt(user_prompt,history_info):
-    '''收入多少，各类型支出多少，'''
-    df=history_info
-    df['timestamp'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
-    df["month"]=df["timestamp"].apply(lambda x:x.month)
-    df_spend = df.groupby(["month","income_or_spending"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==0""")
-    df_income= df.groupby(["month","income_or_spending"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==1""")
-    
-    text_all=""  #每月支出和收入
-    for i in range(df_income.shape[0]):
-        income=df_income.iloc[i,:]
-        spend=df_spend.iloc[i,:]
-        text_unit=f"In month {i+1}, the spending is {spend[-1]} and the income is {income[-1]}. "
-        text_all += text_unit
-    
-    df_income_type = df.groupby(["income_or_spending","type"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==1""")
-    df_spend_type = df.groupby(["income_or_spending","type"],as_index=False).agg({"amount":"sum"}).query("""income_or_spending==0""")
-
-    income_type_info='' #不同收入类型
-    for i in range(df_income_type.shape[0]):
-        info=df_income_type.iloc[i,:]
-        type=f'The income type is {info[-2]}, the amount is {info[-1]}.'
-        income_type_info+=type
-
-    spend_type_info='' #不同支出类型
-    for i in range(df_spend_type.shape[0]):
-        info=df_spend_type.iloc[i,:]
-        type=f'The spending type is {info[-2]}, the amount is {info[-1]}.'
-        spend_type_info+=type
-
-    
-    prompt = "here is my monthly income and spending information:" + "\n" + text_all + "\n" + "here is my income type information" + income_type_info + "here is my spend type information" + spend_type_info
-    prompt = "here is my goal: " + user_prompt
-    return prompt
-
-
-
-
-def chat_with_gpt(user_input):
-    
-    '''
-    description: 
-        this function call openai api to make users able to interact with chatgpt within this website
-
-    parameters:
-        None
-    
-    output:
-        print the response from chat-gpt
-
-    '''
-    client = OpenAI(
-    api_key=os.getenv("api_key"),
-    )
-    conversation = [{"role": "system", "content": "You are a helpful assistant. And you need to give the user detailed financial plan to achieve his goal."}]
-    try:
-        conversation.append({"role": "user", "content": user_input})
-        chat_completion = client.chat.completions.create(
-            messages=conversation,
-            model="gpt-4o-mini",
-                    )
-        assistant_reply = chat_completion.choices[0].message.content
-        conversation.append({"role": "assistant", "content": assistant_reply})
-        return assistant_reply
-
-    except TimeoutError:
-        print("error")
-
-
-
-def load_data():
-    df = create_sample_data()
     df['timestamp'] = pd.to_datetime(df['timestamp'])
     df['month'] = df['timestamp'].dt.month
     return df
